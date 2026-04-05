e8700236b53540208accb740d580b0a9907ef7cc
Mark Wade <wade.mark.a@gmail.com>
2026-04-05 08:10:18 -0600

    Formated trip cards wit hthe stop information. May need more formating in the future?

--- src/Pages/Private/MobileItemView/MobileItemView.tsx
+++ src/Pages/Private/MobileItemView/MobileItemView.tsx
@@ -36,13 +36,12 @@ const MobileItemView = () =>{
         }else{
             setDisableBack(false)
         }
-
         if(cardNumber === stops.length ){
             setDisableForward(true)
         }else{
             setDisableForward(false)
         }
-    },[cardNumber])
+    },[cardNumber, stops])
 
     const handleForward = () =>{
         setCardNumber(cardNumber + 1)
@@ -56,6 +55,7 @@ const MobileItemView = () =>{
         <div className="mobile-view-wrapper">
             <button className="std-button back-button" onClick={()=>handleBack()} disabled={disableBack}>Back</button>
             {cardNumber === 0 ? <CardZero selectedTrip={selectedTrip}/> : null}
+            {cardNumber > 0 ? <Cards stops={stops} cardNumber={cardNumber}/> : null}
             <button className="std-button forward-button" onClick={()=>handleForward()} disabled={disableForward}>Forward</button>
         </div>
     )
@@ -69,22 +69,53 @@ const CardZero =({selectedTrip}: {selectedTrip: any})=>{
     return(
         <div className='mobile-view-card-zero'>
             <div className='mobile-view-next-stop-card'>
-                <h3>{selectedTrip.trip_title}</h3>
-                <h4>Planned: {selectedTrip.status_date}</h4>
+                <h3>{selectedTrip?.trip_title}</h3>
+                <h4>Planned: {selectedTrip?.planned_date}</h4>
                 <div className='mobile-view-next-stop-card-content'>
-                    {selectedTrip.summary}
+                    {selectedTrip?.summary}
                 </div>
                 
             </div>
             <div className='mobile-view-stats'>
                 <h3>Stats</h3>
                 <div className='mobile-view-stats-content'>
-                    <h4>Budget: ${selectedTrip.budget}</h4>
-                    <h4>Duration: {convertSecondsToHoursMinutes(selectedTrip.duration)}</h4> 
-                    <h4 className='mobile-view-stats-content-distance'>Distance: {convertMetersToMiles(selectedTrip.distance)}</h4>
+                    <h4>Budget: ${selectedTrip?.budget}</h4>
+                    <h4>Duration: {convertSecondsToHoursMinutes(selectedTrip?.duration)}</h4> 
+                    <h4 className='mobile-view-stats-content-distance'>Distance: {convertMetersToMiles(selectedTrip?.distance)}</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+//Remaining cards HTML
+const Cards = ({stops, cardNumber}: {stops: any[], cardNumber: number})=>{
+    const stop = stops[cardNumber-1]
+    return(
+        <div className='mobile-view-cards'>
+            <div key={stop?.id} className='mobile-view-card '>
+                <h3>{stop?.stop_name}</h3>
+                {cardNumber !== 1 ? <h4>Arrival: {stop?.arrive}</h4> : null}
+                {cardNumber !== 1 && cardNumber !== stops.length ? <h4>Break: {stop?.stay}</h4> : null}
+                <h4 
+                    onClick={() => { if(stop?.location) navigator.clipboard.writeText(stop.location) }}
+                >
+                    Location: {stop?.location ? stop.location.split(',').map((num: string) => parseFloat(num).toFixed(5)).join(', ') : 'Not Set'}
+                </h4>
+                <p style={{textAlign: 'center'}}><i>Touch to copy location</i></p>
+                <h4>Notes:</h4>
+                <h4 className='mobile-view-card-notes'>{stop?.note ? stop.note : 'No Notes'}</h4>
+            </div>
+            <div className='mobile-view-stats-normal'>
+                <h3>Next Stop</h3>
+                <div className='mobile-view-stats-content-stops'>
+                    <h4>Departure: {stop?.depart}</h4>
+                    <h4>Distance: {convertMetersToMiles(stop?.distance_to_next_stop)}</h4>
+                    <h4>Duration: {convertSecondsToHoursMinutes(stop?.time_to_next_stop)}</h4>
                 </div>
                 <button className='std-button google-maps-button'>Directions to Next Stop</button>
             </div>
+            
         </div>
     )
 }
